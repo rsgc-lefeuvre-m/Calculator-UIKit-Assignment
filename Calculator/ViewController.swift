0e0907dc308ae776c66e62c9a08d942d23ff6262
Student <student@macbook-pro-869-lefeuvre.local>
2017-05-13 18:58:07 -0400

    Got numbers 1-9 working.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // This is a connection to the label in our view
+    @IBOutlet weak var labelDisplay: UILabel!
+    
+    // Create an instance (object) of the Calculator class
+    var model = Calculator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Clear the contents of the label
+        labelDisplay.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,70 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onePressed(_ sender: Any) {
+        model.addToNewValue(digit: "1")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func twoPressed(_ sender: Any) {
+        model.addToNewValue(digit: "2")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func threePressed(_ sender: Any) {
+        model.addToNewValue(digit: "3")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func fourPressed(_ sender: Any) {
+        model.addToNewValue(digit: "4")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func fivePressed(_ sender: Any) {
+        model.addToNewValue(digit: "5")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func sixPressed(_ sender: Any) {
+        model.addToNewValue(digit: "6")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func sevenPressed(_ sender: Any) {
+        model.addToNewValue(digit: "7")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func eightPressed(_ sender: Any) {
+        model.addToNewValue(digit: "8")
+        labelDisplay.text = model.providedValue
+    }
+    @IBAction func ninePressed(_ sender: Any) {
+        model.addToNewValue(digit: "9")
+        labelDisplay.text = model.providedValue
+    }
+    
+    @IBAction func zeroPressed(_ sender: Any) {
+        model.addToNewValue(digit: "0")
+        labelDisplay.text = model.providedValue
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+        model.multiply()
+    }
+    
+    @IBAction func equals(_ sender: Any) {
+        model.equals()
+        if model.computedValue != nil {
+            labelDisplay.text = String(format: "%g", model.computedValue!)
+        }
+        
+    }
+    
+    @IBAction func division(_ sender: Any) {
+        model.divide()
+        
+    }
 
-}
-
+    @IBAction func clear(_ sender: Any) {
+        model.clear()
+        
+    }
+    
+    
+    }
+    
